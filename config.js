f89661110a63ecba1f4daec453cd125bc1dd3191
Jose Berrio <aldarisbm@gmail.com>
2018-12-31 09:41:50 -0600

    working maybe

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Where Is East', // Required
+  author: 'Where Is East', // Required
+  description: 'Academic, Mum, Aspiring Coder, Traveler, Mum',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: '',
+    github: '',
+    twitter: '',
+    linkedin: '',
+  },
+  pathPrefix: '/blog',
+  siteUrl: 'http://www.whereiseast.com',
+};
